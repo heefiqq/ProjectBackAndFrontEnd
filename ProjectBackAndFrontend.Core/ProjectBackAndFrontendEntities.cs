671e89eb1477ae490440ad015e808f644c30e4ca
heef <heefiqq@gmail.com>
2020-11-06 22:09:56 +0400

    [+] create project
    Создание проекта

---
+++ ProjectBackAndFrontend.Core/ProjectBackAndFrontendEntities.cs
@@ -0,0 +1,12 @@
+﻿using System.Data.Entity;
+
+namespace ProjectBackAndFrontend.Core
+{
+    class ProjectBackAndFrontendEntities : DbContext
+    {
+
+        public ProjectBackAndFrontendEntities() : base("name=ProjectBackAndFrontendEntities")
+        {
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,6 @@
+# Проект с беком и фронтом
+
+### Структура
+
+* ProjectBackAndFrontend.Core - инфраструктура + работа с базой данных
+* ProjectBackAndFrontend.Web - web-сайт
