759648f57ff40adad5c46a9dbfc9ae2116505c7d
heef <heefiqq@gmail.com>
2020-11-09 23:05:44 +0400

    [feat] paging
    Добавлен код для пагинации

---
+++ ProjectBackAndFrontend.Core/Extensions/QueryExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ProjectBackAndFrontend.Core.Extensions
+{
+    public static class QueryExtensions
+    {
+        public static Paging.FilterResult<T> ToPagedList<T>(this IEnumerable<T> items, int currentPageIndex, int itemsPerPage)
+        {
+            var model = new Paging.FilterResult<T>();
+
+            model.PageIndex = currentPageIndex;
+            model.TotalItemsCount = items.Count();
+            model.TotalPageCount = (int)(Math.Ceiling((double)model.TotalItemsCount / itemsPerPage));
+
+            if (model.TotalPageCount < currentPageIndex && currentPageIndex > 1)
+            {
+                return model;
+            }
+
+            if (currentPageIndex > 1)
+                items = items.Skip((currentPageIndex - 1) * itemsPerPage);
+
+            model.DataItems = items.Take(itemsPerPage).ToList();
+
+            return model;
+        }
+    }
+}
---
+++ ProjectBackAndFrontend.Core/Paging/FilterResult.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectBackAndFrontend.Core.Paging
+{
+    public class FilterResult<TModel>
+    {
+        public List<TModel> DataItems { get; set; }
+
+        public int TotalPageCount { get; set; }
+
+        public int TotalItemsCount { get; set; }
+
+        public int PageIndex { get; set; }
+
+        public string TotalString { get; set; }
+    }
+}
